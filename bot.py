058bd10cd29f9225149c299b7e37ab31f18bb572
Alex <awalker2@stevens.edu>
2025-02-21 00:25:14 -0500

    Improving discord jail code, finds automatically create jail channel if needed and simpler search for member to jail

--- bot.py
+++ bot.py
@@ -24,10 +24,10 @@ async def on_ready():
 async def mute_roulette(ctx: commands.context.Context):
     wait_time = 60
     wait_time_warning = 10
-    await ctx.send(f"Welcome to Mute roulette! Starting in {wait_time} seconds...")
+    await ctx.send(f"Welcome to Mute Roulette! Starting in {wait_time} seconds...")
     await asyncio.sleep(wait_time - wait_time_warning)
 
-    await ctx.send(f"Mute roulette in {wait_time_warning} seconds...")
+    await ctx.send(f"Mute Roulette in {wait_time_warning} seconds...")
     await asyncio.sleep(wait_time_warning)
 
     # Get a random member in voice
@@ -45,30 +45,28 @@ async def mute_roulette(ctx: commands.context.Context):
     await asyncio.sleep(wait_time)
     await member.edit(mute=False)
 
-@bot.command(name="discord-jail", help="Puts a user in jail")
-async def mute_roulette(ctx: commands.context.Context, arg):
-    timeout_time = 60
+@bot.command(name="discord-jail", help="Puts a user in discord-jail, default of 60 seconds")
+async def discord_jail(ctx: commands.context.Context, name, timeout_time = 60):
+    member = discord.utils.get(ctx.guild.members, name=name)
+    if not member or not member.voice.channel:
+        await ctx.send(f'Not so fast there {ctx.author.name}, user has to be in the server and in voice.')
+        return
 
     channel = discord.utils.get(ctx.guild.channels, name="discord-jail")
     if not channel:
-        await ctx.send(f'Not so fast there {ctx.author.name}, we need a "discord-jail" voice channel to do timeouts.')
-        return
+        await ctx.send(f"Oh look {ctx.author.name}, we don't have a discord-jail voice channel for punishments, let me make one.")
+        channel = await ctx.guild.create_voice_channel("discord-jail", category=member.voice.channel.category)
 
-    for voice_channel in ctx.guild.voice_channels:
-        for member in voice_channel.members:
-            if member.name == arg:
-                for x in range(timeout_time):
-                    try:
-                        await member.move_to(channel)
-                    except:
-                        logger.exception("Exception moving user to voice:")
-                    await asyncio.sleep(1)
-    
-    await ctx.send(f'Not so fast there {ctx.author.name}, user has to be in voice.')
-    
+    await ctx.send(f'As you wish {ctx.author.name}, {member.name} has been very bad and is going to discord jail for a punishment of {timeout_time} seconds.')
+    for x in range(timeout_time):
+        try:
+            await member.move_to(channel)
+        except:
+            logger.exception("Exception moving user to voice:")
+        await asyncio.sleep(1)
 
 @bot.command(name="ping", help="Checks that the bot is up and running")
-async def mute_roulette(ctx: commands.context.Context):
+async def ping(ctx: commands.context.Context):
     await ctx.send("pong")
 
 bot.run(token)
