8cd3db98f1b385f8367fbcd632c89448613ff2cb
Alex <awalker2@stevens.edu>
2025-02-22 15:47:47 -0500

    Improving discord jail function to respond to voice state events instead of repeatedly moving the user

--- bot.py
+++ bot.py
@@ -14,11 +14,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+discord_jail_members = {}
+
 @bot.event
 async def on_ready():
-    logger.info(f'Logged on as {bot.user.name} in the following guilds!')
+    logger.info(f'Logged on as {bot.user.name} in the following guilds:')
     for guild in bot.guilds:
         logger.info(guild.name)
+
 
 @bot.command(name="mute-roulette", help="Mutes a random user in voice")
 async def mute_roulette(ctx: commands.context.Context):
@@ -45,28 +48,42 @@ async def mute_roulette(ctx: commands.context.Context):
     await asyncio.sleep(wait_time)
     await member.edit(mute=False)
 
-@bot.command(name="discord-jail", help="Puts a user in discord-jail, default of 60 seconds")
+
+@bot.command(name="discord-jail", help="Puts a user in discord-jail voice channel, default of 60 seconds")
 async def discord_jail(ctx: commands.context.Context, name, timeout_time = 60):
     member = discord.utils.get(ctx.guild.members, name=name)
     if not member or not member.voice.channel:
         await ctx.send(f'Not so fast there {ctx.author.name}, user has to be in the server and in voice.')
         return
 
-    channel = discord.utils.get(ctx.guild.channels, name="discord-jail")
-    if not channel:
+    jail_channel = discord.utils.get(ctx.guild.channels, name="discord-jail")
+    if not jail_channel:
         await ctx.send(f"Oh look {ctx.author.name}, we don't have a discord-jail voice channel for punishments, let me make one.")
-        channel = await ctx.guild.create_voice_channel("discord-jail", category=member.voice.channel.category)
+        jail_channel = await ctx.guild.create_voice_channel("discord-jail", category=member.voice.channel.category)
 
     await ctx.send(f'As you wish {ctx.author.name}, {member.name} has been very bad and is going to discord jail for a punishment of {timeout_time} seconds.')
-    for x in range(timeout_time):
-        try:
-            await member.move_to(channel)
-        except:
-            logger.exception("Exception moving user to voice:")
-        await asyncio.sleep(1)
+    await member.send(f"RIP {member.name}, you have been sent to discord-jail in {ctx.guild.name}, you can leave in {timeout_time} seconds.")
+    
+    # Put the member in jail and wait before removing their role
+    await member.move_to(jail_channel)
+    discord_jail_members[member] = jail_channel
+    await asyncio.sleep(timeout_time)
+    del discord_jail_members[member]
+
+    await ctx.send(f'{ctx.author.name}, {member.name} is no longer sentenced to discord jail.')
+    await member.send(f"{member.name}, you are no longer sentenced to discord jail in {ctx.guild.name}.")
+
+# This is to keep users in discord-jail in the discord-jail channel if they try to join another channel
+@bot.event
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    jail_channel = discord_jail_members.get(member)
+    if jail_channel and after.channel != jail_channel:
+        await member.move_to(jail_channel)
+
 
 @bot.command(name="ping", help="Checks that the bot is up and running")
 async def ping(ctx: commands.context.Context):
     await ctx.send("pong")
+
 
 bot.run(token)
